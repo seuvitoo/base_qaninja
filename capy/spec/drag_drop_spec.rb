3489934814f73b92e0908bcf2d3d0d9ecc6a42cf
Victor Santos <vy.santos@live.com>
2019-10-31 20:18:01 -0300

    Version pc (#8)
    * Finalizando Modulo RSPEC

--- capy/spec/drag_drop_spec.rb
+++ capy/spec/drag_drop_spec.rb
@@ -10,6 +10,7 @@ describe "Drag and Drop", :drag do
     spiderman = find("img[alt$=Aranha]")
     spiderman.drag_to stark
     expect(stark).to have_css ("img[alt$=Aranha]")
+
     expect(cap).not_to have_css ("img[alt$=Aranha]")
   end
 
